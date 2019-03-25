33d6a4efb8d4c4fced8ef1dbdec7311af1c805de
Prashant <pras14jan@gmail.com>
2019-03-26 01:00:03 +0530

    m commit

---
+++ ClassLibraryFizzBuzz/PlainNumberCatagorizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace MVCFizzBuzz.ClassLibraryFizzBuzz
+{
+    public class PlainNumberCatagorizer : AbstractFizzBuzz, IFizzBuzzCatagorizer
+    {
+        public override string IsSatisfiedBy(int number)
+        {
+            return number.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFizzBuzz.Models;
+using MVCFizzBuzz.ClassLibraryFizzBuzz;
+
+namespace MVCFizzBuzz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFizzBuzzResult _fizzBuzzResult;
+
+        public HomeController(IFizzBuzzResult fizzBuzzResult)
+        {
+            this._fizzBuzzResult = fizzBuzzResult;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(FizzBuzzModel fizzBuzzModel)
+        {
+           
+
+            fizzBuzzModel.FizzBuzzList = _fizzBuzzResult.ExecuteCalculate(fizzBuzzModel.Num);
+            
+
+            return View("FizzBuzzView", fizzBuzzModel);
+        }
+
+
+        
+    }
+}
