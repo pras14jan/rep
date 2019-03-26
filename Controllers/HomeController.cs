71bc35d3ee9abfebe2b1fa84bd99c24fb1580d1a
Prashant <pras14jan@gmail.com>
2019-03-26 22:43:43 +0530

    ClassLibrary

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCFizzBuzz.Models;
-using MVCFizzBuzz.ClassLibraryFizzBuzz;
+using ServicesFizzBuzz.FizzBuzzBusinessLogic;
 
 namespace MVCFizzBuzz.Controllers
 {
